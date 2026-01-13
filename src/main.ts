5ca2ede35b68eb8d4084943f2deb20fa0a9f9993
Juan Felipe Tapasco <felipe-08011@hotmail.com>
2026-01-13 18:44:02 -0500

    update port

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT);
+  console.log(`Application is running on: ${process.env.PORT}`);
 }
 bootstrap();
