697b02fa24002d7bc8417f145660d14633af9662
Juan Felipe Tapasco <felipe-08011@hotmail.com>
2026-01-13 19:20:23 -0500

    fix: limit Node memory usage for Render deployment

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,8 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT);
-  console.log(`Application is running on: ${process.env.PORT}`);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${port}`);
 }
 bootstrap();
